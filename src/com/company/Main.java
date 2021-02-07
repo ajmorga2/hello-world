a381429199c365692f770c13fb1d326ccda678cc
ajmorga2 <ainsleyjmorgan@gmail.com>
2021-02-07 15:29:01 -0500

    addtional output
    adding additional output "program modified...."

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
     }
 }
