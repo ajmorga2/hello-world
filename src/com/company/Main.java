792d9d4f2a00d4ccda597722fde69593775635ad
ajmorga2 <ainsleyjmorgan@gmail.com>
2021-02-07 15:33:58 -0500

    Revert "addtional output"
    This reverts commit a381429199c365692f770c13fb1d326ccda678cc.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
     }
 }
